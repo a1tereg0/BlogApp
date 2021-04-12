ff35e6e45fcef87deacb3d95d29c9cc23809f6ea
Prashant Dahal <prashantdah41@gmail.com>
2021-04-12 01:56:59 -0400

    Use Entity Framework and create data context

---
+++ BlogApp/Context/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApp.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+
+    }
+}
