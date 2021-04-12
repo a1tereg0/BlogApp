256c2d11bba0e3564ecc6ac46e9a4c2cbd321da3
Prashant Dahal <prashantdah41@gmail.com>
2021-04-12 01:49:00 -0400

    Create Blog and Author models

---
+++ BlogApp/Models/Author.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApp.Models
+{
+    public class Author
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AuthorID { get; set; }
+
+        [Display(Name = "First Name")]
+        [StringLength(20, ErrorMessage = "Your first name cannot exceed 20 characters")]
+        [Required]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [StringLength(20, ErrorMessage = "Your last name cannot exceed 20 characters")]
+        [Required]
+        public string LastName { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime Birthday { get; set; }
+
+        public byte Age => (byte)(DateTime.Now.Year - Birthday.Year);
+
+        [Display(Name = "Email Address")]
+        [EmailAddress]
+        [Required]
+        public string EmailAddress { get; set; }
+
+        [Display(Name = "Web site")]
+        [Url]
+        public string WebAddress { get; set; }
+
+        [Display(Name = "Phone Number")]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        public string Country { get; set; }
+
+        public string Province { get; set; }
+
+        [Display(Name = "Postal/Zip Code")]
+        [DataType(DataType.PostalCode)]
+        public string PostalCode { get; set; }
+
+        public ICollection<Blog> blogs;
+
+    }
+}
---
+++ BlogApp/Models/Blog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApp.Models
+{
+    public class Blog
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BlogID { get; set; }
+
+        [StringLength(60, ErrorMessage = "You title should not exceed sixty characters.")]
+        [Required]
+        public string Title { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        [Required]
+
+        [Display(Name = "Content")]
+        public string Body { get; set; }
+
+
+        public int AuthorID { get; set; }
+
+        [ForeignKey("AuthorID")]
+        public Author Author { get; set; }
+
+        [Editable(false)]
+        public DateTime PublishDate { get; set; } = DateTime.Now;
+
+    }
+}
