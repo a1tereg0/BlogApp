e162354df3faeb272e60b7bc4a99f39ddee64b7b
Prashant Dahal <prashantdah41@gmail.com>
2021-04-12 18:58:34 -0400

    Implement BlogPages to show a single blog and use Cache API to save blog and Inter-App sharing API to share blog

--- BlogApp/Pages/BlogPost.cshtml.cs
+++ BlogApp/Pages/BlogPost.cshtml.cs
@@ -4,13 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BlogApp.Models;
 
 namespace BlogApp.Pages
 {
     public class BlogPostModel : PageModel
     {
-        public void OnGet()
+        private readonly BlogApp.Context.DataContext _context;
+
+        public Blog Blog { get; set; }
+        public BlogPostModel(BlogApp.Context.DataContext context)
         {
+            _context = context;
+        }
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Blog = await _context.Blogs.Include(b => b.Author).FirstOrDefaultAsync(b => b.BlogID == id);
+
+            if (Blog == null)
+            {
+                return NotFound();
+            }
+            return Page();
         }
     }
 }
